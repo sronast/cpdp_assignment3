953fee75f1561376aa4c22e8723a20ebe7330233
Ronast - Subedi <subedi@linprog1.cs.fsu.edu>
2024-03-11 20:00:28 -0400

    seperate into header and cpp files

---
+++ game.hpp
@@ -0,0 +1,27 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include<iostream>
+#include<vector>
+#include<map>
+
+class TicTacToe {
+private:
+    std::vector<std::vector<char>> board;
+    const int boardSize = 3;  
+    std::map<char, int> columnMap{{'A', 0}, {'B', 1}, {'C', 2}}; 
+
+public:
+    TicTacToe() {
+        resetBoard();
+    }
+    void resetBoard();
+
+    void displayBoard() const;
+
+    bool makeMove(char column, int row, char symbol);
+
+    bool checkGameWon(char symbol) const;
+};
+
+#endif
